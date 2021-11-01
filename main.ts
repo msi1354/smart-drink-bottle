cf8f887ad3a56c09445b6c5e83ccfd59d111027d
msi1354 <86691276+msi1354@users.noreply.github.com>
2021-11-02 11:30:42 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 let time = 0
 basic.forever(function () {
-    if (input.magneticForce(Dimension.X) > -130) {
+    if (input.magneticForce(Dimension.X) > 30) {
         time = 5
         while (time > 0) {
             time += 0 - 1
@@ -8,11 +8,14 @@ basic.forever(function () {
         }
         while (time == 0) {
             basic.showIcon(IconNames.Sad)
+            music.playMelody("B A C5 B A C5 B A ", 250)
+            basic.showNumber(input.magneticForce(Dimension.X))
             basic.pause(2000)
             control.reset()
         }
     } else {
         basic.showIcon(IconNames.Happy)
+        basic.showNumber(input.magneticForce(Dimension.X))
         basic.pause(2000)
         control.reset()
     }
