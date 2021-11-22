54467c1f363f287273a0b23cc6baf3b55312a70f
msi1354 <86691276+msi1354@users.noreply.github.com>
2021-11-23 09:07:58 +1300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,16 @@ let time = 0
 radio.setGroup(55)
 basic.forever(function () {
     if (input.magneticForce(Dimension.X) > 30) {
-        time = 10
+        time = 11
         while (time > 0 && input.magneticForce(Dimension.X) > 30) {
             time += 0 - 1
             basic.showNumber(time)
+            while (time == 5 && input.magneticForce(Dimension.X) > 30) {
+                basic.showString("Drink")
+                radio.sendNumber(2)
+            }
             while (time == 0 && input.magneticForce(Dimension.X) > 30) {
-                basic.showIcon(IconNames.Sad)
+                basic.showString("Drink!")
                 music.playTone(262, music.beat(BeatFraction.Whole))
                 radio.sendNumber(1)
             }
